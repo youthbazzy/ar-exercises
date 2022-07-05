0ba1f5ca111d0f7e5ac51cff78cb4104426dd303
Bazil <king.bazil.khan@gmail.com>
2022-07-05 20:10:57 +0000

    Completes exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+
+@store2 = Store.find_by(id: 2)
+
+puts @store1.name
+puts @store2.name
+
+@store1.name = "Mikey"
+@store1.save
+
+puts @store1.name
