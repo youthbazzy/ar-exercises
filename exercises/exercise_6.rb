866f6c22ffc5f8b8639c0f3aaf8a37d7dc4d81ce
Bazil <king.bazil.khan@gmail.com>
2022-07-05 20:25:09 +0000

    Completes exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Banana", last_name: "First", hourly_rate: 100)
+@store1.employees.create(first_name: "Micheal", last_name: "First", hourly_rate: 95)
+@store1.employees.create(first_name: "Bazil", last_name: "First", hourly_rate: 90)
+
+@store2.employees.create(first_name: "Banana", last_name: "Second", hourly_rate: 100)
+@store2.employees.create(first_name: "Micheal", last_name: "Second", hourly_rate: 95)
+@store2.employees.create(first_name: "Bazil", last_name: "Second", hourly_rate: 90)
